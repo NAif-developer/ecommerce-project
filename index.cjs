4adf8ec1a1df3846305d26304788addbf05c2862
NAif-developer <naif.mutairi2003@gmail.com>
2026-06-24 10:51:40 +0300

    Update index.cjs

--- index.cjs
+++ index.cjs
@@ -20,7 +20,11 @@ async function start() {
     await mongoose.connect(process.env.MONGO_URI, { serverSelectionTimeoutMS: 5000 });
 
     console.log('Connected to Database!');
-    app.listen(5000, () => console.log('Server running on port 5000'));
+   const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
   } catch (err) {
     console.error('Database connection failed:', err.message);
   }
